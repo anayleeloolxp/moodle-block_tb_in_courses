2fc3c9f687c2e0b900b11f909983b85fdcf1127e
Anay <anay.infowind@gmail.com>
2020-11-23 15:14:57 +0530

    Added autoslide setting

--- renderer.php
+++ renderer.php
@@ -47,7 +47,11 @@ class block_tb_in_courses_renderer extends plugin_renderer_base {
         if($config->inprogress_showasslider == 1){
             $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/js/jquery.min.js'));
             $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/js/owl.carousel.js'));
-            $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/js/owlslider.js'));
+            if($config->inprogress_autoslide == 1){
+                $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/js/owlslider-auto.js'));
+            }else{
+                $this->page->requires->js(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/js/owlslider.js'));
+            }    
 
             $this->page->requires->css(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/css/owl.carousel.min.css'));
             $this->page->requires->css(new moodle_url($CFG->wwwroot . '/blocks/tb_in_courses/css/owl.theme.default.min.css'));
