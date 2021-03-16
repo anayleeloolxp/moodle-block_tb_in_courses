9d865eebe23961794f9b97e6206954e2e7918f50
Anay <anay.infowind@gmail.com>
2021-03-16 17:12:31 +0530

    Added block title

--- lang/en/block_tb_in_courses.php
+++ lang/en/block_tb_in_courses.php
@@ -109,7 +109,7 @@ $string['nocompletion'] = "Completion not enabled";
 
 $string['privacy:metadata'] = 'The Leeloo LXP In Progress Courses (tb_in_courses) block only shows information about courses and does not store data itself.';
 
-$string['blocktitle'] = 'Block Title';
+$string['blocktitle'] = 'Leeloo LXP In Progress Courses';
 $string['categoryid'] = 'Category Id';
 
 $string['nolicense'] = 'Invalid Leeloo LXP License ID';
